ea8fa822d460fe650683c1b0756debca1b44fb87
cuamurre <eugene.murray@gmail.com>
2018-10-20 22:54:42 +0100

    update emcloud.co

--- emcloud.co/js/site.js
+++ emcloud.co/js/site.js
@@ -1,10 +1,11 @@
+let urlParam = document.currentScript.getAttribute('url-param');
+console.log( "urlParam: ", urlParam);
 
 $( document ).ready(function() {
     console.log( "document ready!" );
-    
-    $( ".container-fluid").hide();
-    $( "#header" ).load( "./Templates/_Header.html", () => {
-        $( ".container-fluid").show();
-    });
-    $( "#footer" ).load( "./Templates/_Footer.html");
+	$( "#header" ).load( `${urlParam}_header.html`, () => {
+		$( "#loading").remove();
+		$( ".container-fluid").show();
+	});
+	$( "#footer" ).load( `${urlParam}_footer.html`);
 });
